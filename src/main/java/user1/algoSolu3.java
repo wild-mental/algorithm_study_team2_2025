eca6d32204bd5eaefceef87cf39f2eb6800e409f
wild-mental <wildmental@knou.ac.kr>
2025-01-22 11:23:55 +0900

    유저 1의 핵심 공유 코드 (버전2)

---
+++ src/main/java/user1/algoSolu3.java
@@ -0,0 +1,19 @@
+package user1;
+
+public class algoSolu3 {
+    // 유저 1의 핵심 공유 코드 (버전2)
+    // 어떤 기능을 구현하는데
+    // 여러가지 방식으로 구현할 수 있음
+    // 그중 하나를 선택해서 버전1 작성
+    // 이후 갑자기 변심해서 버전2로 변경하고
+    // 핵심 메서드 시그니처 수정 후
+    // 기존 내용이 부끄러워서 Force Push 하고 싶음
+    public void myImportantCoreMethod(
+        Integer importantParameter1,
+        String importantParameter2
+    ){
+        // 다른 곳에서 매우 많이 호출하며
+        // 연관작업을 수행해야 하는 코드
+    }
+
+}
