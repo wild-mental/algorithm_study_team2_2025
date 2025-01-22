790d3fce1a8ac8defa6b1e368cd2f350d115cf0c
wild-mental <wildmental@knou.ac.kr>
2025-01-22 11:26:10 +0900

    유저 2의 작업 코드 (유저 1의 핵심코드 활용 및 확장)

--- src/main/java/user1/algoSolu3.java
+++ src/main/java/user1/algoSolu3.java
@@ -12,4 +12,8 @@ public class algoSolu3 {
         // 연관작업을 수행해야 하는 코드
     }
 
+    public static void main(String[] args) {
+        algoSolu3 algo = new algoSolu3();
+        algo.myImportantCoreMethod("아주 중요하고 방대한 작업");
+    }
 }
